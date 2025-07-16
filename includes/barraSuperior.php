62b19bd991084abdcc9b8196c3bca9a207d02af8
joseangelcasabona <joseangelcasabona@gmail.com>
2025-07-16 14:20:49 +0200

    Delete includes directory

--- includes/barraSuperior.php
+++
@@ -1,7 +0,0 @@
-<div class="barra-superior">
-    <div class="acciones-ventana">
-        <span class="min"><i class="fas fa-window-minimize"></i></span>
-        <span class="max"><i class="far fa-square"></i></span>
-        <span class="close"><i class="fas fa-times"></i></span>
-    </div>
-</div>
